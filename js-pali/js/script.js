a82a9152de35ee8aebef65c97733a796cb769349
Nicola <nicola.ba1989@gmail.com>
2020-01-13 16:15:59 +0100

    update pali

--- js-pali/js/script.js
+++ js-pali/js/script.js
@@ -1,4 +1,4 @@
-var parolaUser = prompt('Inserisci una parola (non una frase):');
+var parolaUser = prompt('Inserisci una parola (non una frase, senza punteggiatura, spazi e numeri!):');
 
 if (is_pali(parolaUser)) {
     alert('La parola è palindroma.');
@@ -7,13 +7,15 @@ else {
     alert('La parola non è palindroma.');
 }
 
-// parola di partenza, array dello spelling, reverse, unisci e controlli se è uguale a alla porola di partenza.
+// parola di partenza, finchè la prima dell'indice è uguale all'ultima(facendo la lunghezza della parola supponiamo 5 sottrai 1 quindi 4 e sottrai la lunghezza della parola per andare in negativo con l'indice quindi al contrario) il loop andrà avanti e restituisce vero, come una lettera è diversa si interrompe ed è false.
 // / ***********************************************
-function is_pali(parola) {
-    if (parola.toLowerCase().split("").reverse().join("") === parola) {
-        return true;
-    }
-    else {
+
+function is_pali(str) {
+ for (var i = 0; i < str.length; i++) {
+   if (str[i] !== str[str.length - 1 - i]) {
+        console.log(str[i] + ' --> diverso da --> ' + str[str.length - 1 - i]);
         return false;
-    }
+   }
+ }
+ return true;
 }
